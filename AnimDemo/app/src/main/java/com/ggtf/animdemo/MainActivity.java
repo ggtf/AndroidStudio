aca8817a4e446f577643aaba52cb1a84dc76c29a
ggtf <15170069952@163.com>
2015-09-26 12:56:06 +0800

    View Animation

---
+++ AnimDemo/app/src/main/java/com/ggtf/animdemo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.ggtf.animdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView imageView = (ImageView) findViewById(R.id.image_view);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.anim_list);
+        imageView.startAnimation(animation);
+    }
+
+}
